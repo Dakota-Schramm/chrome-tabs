c7cccc2a1605fa330808f63c58de2e3a24359044
Dakota Schramm <william.schramm11@gmail.com>
2023-09-26 23:10:09 -0500

    Add better titles

--- popup.js
+++ popup.js
@@ -17,56 +17,85 @@
 // Group tabs by creating a tab group with all tabs that match the URL pattern
 // Pattern should be based on domain
 
-const tabs = await chrome.tabs.query({
-  windowId: chrome.windows.WINDOW_ID_CURRENT
-})
+const tabs = await getOrderedTabs();
 const groups = createGroups(tabs);
 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Collator
-const collator = new Intl.Collator();
-tabs.sort((a, b) => collator.compare(a.title, b.title));
-
 const template = document.getElementById('li_template');
-const elements = new Set();
-for (const tab of tabs) {
-  const element = template.content.firstElementChild.cloneNode(true);
+const tabList = createTabList(template);
+document.querySelector('ul').append(...tabList);
 
-  const title = tab.title.split('-')[0].trim();
-  const pathname = new URL(tab.url).pathname.slice('/docs'.length);
-
-  element.querySelector('.title').textContent = title;
-  element.querySelector('.pathname').textContent = pathname;
-  element.querySelector('a').addEventListener('click', async () => {
-    // need to focus window as well as the active tab
-    await chrome.tabs.update(tab.id, { active: true });
-    await chrome.windows.update(tab.windowId, { focused: true });
-  });
-
-  elements.add(element);
-}
-document.querySelector('ul').append(...elements);
-
-const button = document.querySelector('button');
-button.addEventListener('click', async () => {
+const buttonGroup = document.querySelector('button#group');
+buttonGroup.addEventListener('click', async () => {
   for (const pair of groups.entries()) {
     const [hostname, tabs] = pair;
+
     if (!tabs.length) continue
 
     const tabIds = tabs.map(tab => tab.id);
     const group = await chrome.tabs.group({ tabIds });
-    await chrome.tabGroups.update(group, { title: hostname });
+
+    const title = getTitleFromHostName(hostname);
+    await chrome.tabGroups
+      .update(group, { collapsed: true, title });
   }
 });
+
+async function getOrderedTabs() {
+  const tabs = await chrome.tabs.query({
+    windowId: chrome.windows.WINDOW_ID_CURRENT
+  })
+
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Collator
+  const collator = new Intl.Collator();
+  tabs.sort((a, b) => collator.compare(a.title, b.title));
+
+  return tabs
+}
 
 function createGroups(tabs) {
   const groups = new Map();
 
   tabs.forEach(tab => {
-    const { hostname } = new URL(tab.url);
+    const { hostname, ...rest } = new URL(tab.url);
     const group = groups.get(hostname) || [];
     group.push(tab);
     groups.set(hostname, group);
   });
 
   return groups
+}
+
+function createTabList(template) {
+  const elements = new Set();
+  for (const tab of tabs) {
+    const element = template.content.firstElementChild.cloneNode(true);
+
+    const title = tab.title.split('-')[0].trim();
+    const pathname = new URL(tab.url).pathname.slice('/docs'.length);
+
+    element.querySelector('.title').textContent = title;
+    element.querySelector('.pathname').textContent = pathname;
+    element.querySelector('a').addEventListener('click', async () => {
+      // need to focus window as well as the active tab
+      await chrome.tabs.update(tab.id, { active: true });
+      await chrome.windows.update(tab.windowId, { focused: true });
+    });
+
+    elements.add(element);
+  }
+
+  return elements
+}
+
+function getTitleFromHostName(hostname) {
+  const labels               = hostname.split('.')
+  const secondaryLevelDomain = labels.at(-2)
+  const topLevelDomain       = labels.at(-1)
+
+  const insignificantLabels = [ "www", "com", "org" ]
+  const filteredLabels = labels
+    .filter(label => !insignificantLabels.includes(label))
+
+  if (!secondaryLevelDomain) return topLevelDomain
+  return filteredLabels.join('.')
 }
